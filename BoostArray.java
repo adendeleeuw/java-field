ffee7910d0ddce512579c29d98f7c4dc76401467
Aden de Leeuw <62326264+adendeleeuw@users.noreply.github.com>
2021-11-09 20:14:12 +1100

    Create BoostArray.java

---
+++ BoostArray.java
@@ -0,0 +1,44 @@
+public class BoostArray
+{
+
+    private String boostType;
+    private int boostValue;
+
+    public BoostArray()
+    {
+        this.boostType = "undefined";
+        this.boostValue = 0;
+    }
+
+    public BoostArray(String boostType, int boostValue)
+    {
+        this.boostType = boostType;
+        this.boostValue = boostValue;
+    }
+
+    public String toString()
+    {
+        return "\nBoost Type: " + this.boostType + "\nBoost Value: " + this.boostValue;
+    }
+
+    public String getBoostType()
+    {
+        return boostType;
+    }
+
+    public int getBoostValue()
+    {
+        return boostValue;
+    }
+
+    public void setBoostType(String boostType)
+    {
+        this.boostType = boostType;
+    }
+
+    public void setBoostValue(int boostValue)
+    {
+        this.boostValue = boostValue;
+    }
+
+}
