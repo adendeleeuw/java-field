b567034382acbe4e7c0d5a6c00af1aeab00ecb06
Aden de Leeuw <62326264+adendeleeuw@users.noreply.github.com>
2021-11-26 17:08:26 +1100

    Update Capture.java

--- Capture.java
+++ Capture.java
@@ -11,75 +11,67 @@ public class Capture
         this.dice = new Dice(6);
     }
 
-    public Input getInput()
+    public void playCaptureComputer(Player player1, Player player2, Board board)
     {
-        return input;
-    }
-
-
-    public void playCapture(Player player1, Player player2,
-                            Board board, boolean isComputer) 
+        int rows = board.getGrid().getRows();
+        int columns = board.getGrid().getColumns();
+        Random r = new Random();
+        int x = r.nextInt(columns) + 1;
+        int y = r.nextInt(rows) + 1;
+        int arrayIndex = ( columns * ( y - 1 ) + ( x - 1 ));
+        boolean result = calculateStats(player1, player2, columns, true);
+        String arrayValue = board.getGrid().getGridArray()[arrayIndex].getGridValue();
+        if (result)
         {
-            String name = "";
-            int x = 0;
-            int y = 0;
-            int rows = board.getGrid().getRows();
-            int columns = board.getGrid().getColumns();
-            while (true){
-                if (!isComputer)
+            board.getGrid().setGridArrayValue(arrayIndex, player2.getPlayerToken());
+            player1.getPlayerStats().incrementGridSpacesLost();
+            String boostType = board.getGrid().getGridArray()[arrayIndex].getBoostArray().getBoostType();
+            if (!boostType.equals(""))
+            {
+                if (arrayValue.equals(player1.getPlayerToken()))
                 {
-                name = player1.getName();
-                x = this.getInput().acceptInt("\nPlease enter X coordinate");
-                y = this.getInput().acceptInt("\nPlease enter Y coordinate");
-                } 
-                else 
-                {
-                name = player2.getName();
-                Random r = new Random();
-                x = r.nextInt(columns) + 1;
-                y = r.nextInt(rows) + 1;
+                    captureBoost(player1, 
+                    board.getGrid().getGridArray()[arrayIndex].getBoostArray().getBoostType(), 
+                    board.getGrid().getGridArray()[arrayIndex].getBoostArray().getBoostValue(), board, true); 
                 }
-                
-                if ( x <= columns && x > 0 && y <= rows && y > 0)
+                captureBoost(player2, 
+                board.getGrid().getGridArray()[arrayIndex].getBoostArray().getBoostType(), 
+                board.getGrid().getGridArray()[arrayIndex].getBoostArray().getBoostValue(), board, false);
+                if (boostType.equals("Coins"))
                 {
-                    boolean result = this.calculateStats(player1, player2, columns, isComputer);
-                    if (result)
-                    {
-                    int arrayIndex = ( columns * ( y - 1 ) + ( x - 1 ));
-                    String arrayValue = (" " + name.substring(0, 1) + " ");
-                    board.getGrid().setGridArrayValue(arrayIndex, arrayValue);
-                    }
-                    break;
-                } else {
-                    System.out.println("\nNot valid for this map size");
+                    board.getGrid().getGridArray()[arrayIndex].getBoostArray().setBoostType("");
+                    board.getGrid().getGridArray()[arrayIndex].getBoostArray().setBoostValue(0);
                 }
-            } 
-        }
-
+            }
+        }   
+    }
 
     private boolean calculateStats(Player player1, Player player2, int columns, boolean isComputer)
     {
-
-            //calculate attack and defence
-            if (isComputer)
+        //calculate attack and defence
+        if (isComputer)
+        {
+            int player1DefenceValue = (player1.getPlayerStats().getTotalDefence(this.dice.rollDice(3,
+                                                                player1.getName()), player1.getName()));
+            int player2AttackValue = (player2.getPlayerStats().getTotalStrength(this.dice.rollDice(2,
+                                                                player2.getName()), player2.getName()));
+            if (player2AttackValue > player1DefenceValue)
             {
-                int player1DefenceValue = (player1.getPlayerStats().getTotalDefence(this.dice.rollDice(3, player1.getName()), player1.getName()));
-                int player2AttackValue = (player2.getPlayerStats().getTotalStrength(this.dice.rollDice(2, player2.getName()), player2.getName()));
-                if (player2AttackValue > player1DefenceValue)
-                {
-                    System.out.println("\n" + player2.getName() + " wins\n"); 
-                    return true;
-                } else 
-                {
-                    System.out.println("\n" + player1.getName() + " wins\n");
-                    return false; 
-                }
+                System.out.println("\n" + player2.getName() + " wins\n"); 
+                return true;
             } 
             else 
             {
-            int player1AttackValue = (player1.getPlayerStats().getTotalStrength(this.dice.rollDice(3, player1.getName()), player1.getName()));
-            int player2DefenceValue = (player2.getPlayerStats().getTotalDefence(this.dice.rollDice(2, player2.getName()), player2.getName()));
-
+                System.out.println("\n" + player1.getName() + " wins\n");
+                return false; 
+            }
+        } 
+        else 
+        {
+            int player1AttackValue = (player1.getPlayerStats().getTotalStrength(this.dice.rollDice(3,
+                                                                player1.getName()), player1.getName()));
+            int player2DefenceValue = (player2.getPlayerStats().getTotalDefence(this.dice.rollDice(2,
+                                                                player2.getName()), player2.getName()));
             if ( player1AttackValue > player2DefenceValue )
             {
                 System.out.println("\n" + player1.getName() + " wins\n");
@@ -93,5 +85,100 @@ public class Capture
         }
     }
 
-
+    public void captureBoost(Player player, String boostType, int boostValue, Board board, boolean reverse)
+    {
+        if ( boostType == "Strength")
+        {
+            if(reverse)
+            {
+                player.getPlayerStats().setStrength(player.getPlayerStats().getStrength() - boostValue);
+            }
+            else
+            {
+                player.getPlayerStats().setStrength(player.getPlayerStats().getStrength() + boostValue);
+                System.out.println("\n" + player.getName() + " acquired a strength bonus of " + boostValue);
+            }
+        }
+        else if ( boostType == "Defence")
+        {
+            if (reverse)
+            {
+                player.getPlayerStats().setDefence(player.getPlayerStats().getDefence() - boostValue);
+            }
+            else
+            {
+                player.getPlayerStats().setDefence(player.getPlayerStats().getDefence() + boostValue);
+                System.out.println("\n" + player.getName() + " acquired a Defence bonus of " + boostValue);
+            }
+        }
+        else if ( boostType == "Coins")
+        {
+            player.getPlayerStats().setCoins( player.getPlayerStats().getCoins() + boostValue);
+            System.out.println("\n" + player.getName() + " acquired " + boostValue + " coins" );
+        }
     }
+
+    public void playCapture(Player player1, Player player2, Board board) 
+    {
+        int x = 0;
+        int y = 0;
+        int rows = board.getGrid().getRows();
+        int columns = board.getGrid().getColumns();
+        while (true)
+        {
+            x = this.input.acceptInt("\nPlease enter X coordinate");
+            y = this.input.acceptInt("\nPlease enter Y coordinate");
+
+            if ( x <= columns && x > 0 && y <= rows && y > 0)
+            {
+                int arrayIndex = ( columns * ( y - 1 ) + ( x - 1 ));
+                String gridValue = board.getGrid().getGridArray()[arrayIndex].getGridValue();
+                if (!gridValue.equals(player1.getPlayerToken()))
+                    {
+                        boolean result = this.calculateStats(player1, player2, columns, false);
+                        if (result)
+                        {
+                            board.getGrid().setGridArrayValue(arrayIndex, player1.getPlayerToken());
+                            player1.getPlayerStats().incrementGridSpacesCaptured();
+                            String boostType = board.getGrid().getGridArray()[arrayIndex].getBoostArray().getBoostType();
+                            if (!boostType.equals(""))
+                            {
+                                if (gridValue.equals(player2.getPlayerToken()))
+                                {
+                                    captureBoost(player2,
+                                    board.getGrid().getGridArray()[arrayIndex].getBoostArray().getBoostType(), 
+                                    board.getGrid().getGridArray()[arrayIndex].getBoostArray().getBoostValue(), board, true);
+                                }
+
+                                captureBoost(player1,
+                                board.getGrid().getGridArray()[arrayIndex].getBoostArray().getBoostType(), 
+                                board.getGrid().getGridArray()[arrayIndex].getBoostArray().getBoostValue(), board, false);
+                                
+                                if (boostType.equals("Coins"))
+                                {
+                                    board.getGrid().getGridArray()[arrayIndex].getBoostArray().setBoostType("");
+                                    board.getGrid().getGridArray()[arrayIndex].getBoostArray().setBoostValue(0);
+                                }
+
+                            }   
+                         }
+                        break;
+                    } 
+                    else 
+                    {
+                        System.out.println("You cannot capture a grid space you have already captured");
+                    }
+            } 
+            else 
+            {
+                System.out.println("\nNot valid for this map size");
+            }
+        } 
+    }
+
+    public String toString()
+    {
+        return "Dice: " + dice;
+    }
+
+}
