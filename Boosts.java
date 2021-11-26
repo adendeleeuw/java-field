8a30fe6f73635e0b621f8a6321bd76178e2bdb21
Aden de Leeuw <62326264+adendeleeuw@users.noreply.github.com>
2021-11-26 17:08:09 +1100

    Update Boosts.java

--- Boosts.java
+++ Boosts.java
@@ -1,5 +1,4 @@
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -20,41 +19,44 @@ public class Boosts
     return boostArray;
   }
 
-  public void display()
-  {
-    System.out.println(this.boostArray);
-  }
   
   public void parseFile(String FILE_NAME)
-    {
+  {
       try
       {
-        FileReader reader = new FileReader(FILE_NAME);
-        Scanner fileInput = new Scanner(reader);
-        while (fileInput.hasNextLine())
-        {
-          String[] lineContents = fileInput.nextLine().split(",");
-          for (int i = 0; i < lineContents.length; i++ )
+          FileReader reader = new FileReader(FILE_NAME);
+          Scanner fileInput = new Scanner(reader);
+          while (fileInput.hasNextLine())
           {
-            if (i == 0 && !lineContents[0].equals("0"))
+            String[] lineContents = fileInput.nextLine().split(",");
+            for (int i = 0; i < lineContents.length; i++ )
             {
-              this.boostArray.add(new BoostArray("Strength", Integer.parseInt(lineContents[0])));
-            }
-            else if (i == 1 && !lineContents[1].equals("0"))
-            {
-              this.boostArray.add(new BoostArray("Defence", Integer.parseInt(lineContents[1])));
-            }
-            else if (i == 2 && !lineContents[2].equals("0"))
-            {
-              this.boostArray.add(new BoostArray("Coins", Integer.parseInt(lineContents[2])));
+              if (i == 0 && !lineContents[0].equals("0"))
+              {
+                this.boostArray.add(new BoostArray("Strength", Integer.parseInt(lineContents[0])));
+              }
+              else if (i == 1 && !lineContents[1].equals("0"))
+              {
+                this.boostArray.add(new BoostArray("Defence", Integer.parseInt(lineContents[1])));
+              }
+              else if (i == 2 && !lineContents[2].equals("0"))
+              {
+                this.boostArray.add(new BoostArray("Coins", Integer.parseInt(lineContents[2])));
+              }
             }
           }
-        } 
-        
-        }
-        catch (Exception e)
-        {
-          System.out.println(e);
-        }
-    }
+          reader.close();
+          fileInput.close(); 
+      }
+      catch (Exception e)
+      {
+        System.out.println("An error occured while trying to read the file");
+      }
+  }
+
+  public String toString()
+  {
+    return "BoostArray: " + boostArray;
+  }
+
 }
