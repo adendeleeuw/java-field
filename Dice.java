cabfe66c6846f03818878d951041590e49fe48ac
Aden de Leeuw <62326264+adendeleeuw@users.noreply.github.com>
2021-11-26 17:08:41 +1100

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,6 +1,5 @@
 public class Dice
 {
-
     private int diceSides;
 
     public Dice()
@@ -8,37 +7,36 @@ public class Dice
         this.diceSides = 0;
     }
 
-    public Dice(int diceSides){
+    public Dice(int diceSides)
+    {
         this.diceSides = diceSides;
     }
 
-     public int getDiceSides(){
+    public int getDiceSides()
+    {
         return diceSides;
     }
 
     public int rollDice(int times, String name)
     {
-        int dice1;
-        int dice2;
-        int dice3;
+        int sum = 0;
+        for (int i = 0; i < times; i++ )
+        {
+            sum+= (Math.random() * this.diceSides) + 1;
+        }
 
-    if (times == 2)
-    {
-        dice1=(int)(Math.random()*this.diceSides+1);
-        dice2=(int)(Math.random()*this.diceSides+1);
-        System.out.println("\n" + name + " rolled a " + ( dice1 + dice2 ));
-        return dice1 + dice2;
-    } else if (times == 3)
-    {
-        dice1=(int)(Math.random()*this.diceSides+1);
-        dice2=(int)(Math.random()*this.diceSides+1);
-        dice3=(int)(Math.random()*this.diceSides+1);
-        System.out.println(name + " rolled a " + ( dice1 + dice2 + dice3));
-        return dice1 + dice2 + dice3;
-    }
-    return 0;
+        System.out.println("\n" + name + " rolled a " + sum);
+        return sum;
     }
 
-   
+    public void setDiceSides(int diceSides)
+    {
+        this.diceSides = diceSides;
+    }
+
+    public String toString()
+    {
+        return "Dice Sides: " + diceSides;
+    }
 
 }
